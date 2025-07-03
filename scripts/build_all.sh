9dcdf06ce760a511bb2703fa4da7871704f07a20
Distortions81 <carlotto81@gmail.com>
2025-07-03 16:25:08 -0600

    disable darwin

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 mkdir -p dist
 env CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o dist/oni-view-linux-amd64 .
-env CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -o dist/oni-view-darwin-amd64 .
-env CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build -o dist/oni-view-darwin-arm64 .
+#env CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -o dist/oni-view-darwin-amd64 .
+#env CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build -o dist/oni-view-darwin-arm64 .
 env CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o dist/oni-view-windows-amd64.exe .
 env CGO_ENABLED=0 GOOS=js GOARCH=wasm go build -o dist/oni-view.wasm .
 
