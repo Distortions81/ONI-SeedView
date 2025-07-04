babf3e39008c44af0a349440cd617b6368191560
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2025-07-04 10:44:58 -0600

    Add screenshot export menu

--- const.go
+++ const.go
@@ -12,25 +12,27 @@ const (
 	PanSpeed         = 15
 	// CameraMargin controls how far the world can be panned
 	// beyond the visible screen in pixels.
-	CameraMargin        = -64
-	KeyZoomFactor       = 1.05
-	WheelZoomFactor     = 1.1
-	MinZoom             = 0.5
-	IconScale           = 0.2
-	LegendZoomExponent  = 10
-	DefaultWidth        = 1280
-	DefaultHeight       = 720
-	InitialZoom         = 1.0
-	LabelCharWidth      = 6
-	NumberLegendXOffset = 150
-	LegendRowSpacing    = 25
-	ScreenshotFile      = "screenshot.png"
-	HelpIconSize        = 24
-	HelpMargin          = 10
-	CrosshairSize       = 10
-	InfoIconScale       = 0.3
-	InfoPanelAlpha      = 200
-	TouchDragThreshold  = 10
+	CameraMargin          = -64
+	KeyZoomFactor         = 1.05
+	WheelZoomFactor       = 1.1
+	MinZoom               = 0.5
+	IconScale             = 0.2
+	LegendZoomExponent    = 10
+	DefaultWidth          = 1280
+	DefaultHeight         = 720
+	InitialZoom           = 1.0
+	LabelCharWidth        = 6
+	NumberLegendXOffset   = 150
+	LegendRowSpacing      = 25
+	ScreenshotFile        = "screenshot.png"
+	HelpIconSize          = 24
+	HelpMargin            = 10
+	CrosshairSize         = 10
+	InfoIconScale         = 0.3
+	InfoPanelAlpha        = 200
+	TouchDragThreshold    = 10
+	ScreenshotMenuSpacing = 18
+
 	// WheelThrottle controls how often mouse wheel zoom is applied
 	// in WASM to account for faster scroll events.
 	WheelThrottle = 75 * time.Millisecond
