8eb28ee0e74bf38ff3e98ed513bd7e863ad96a89
Distortions81 <carlotto81@gmail.com>
2025-07-05 00:10:19 -0600

    strip binary

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -5,11 +5,11 @@ mkdir -p dist
 #env CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -o dist/oni-view-darwin-amd64 .
 #env CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build -o dist/oni-view-darwin-arm64 .
 #env CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o dist/oni-view-windows-amd64.exe .
-env CGO_ENABLED=0 GOOS=js GOARCH=wasm go build -ldflags="-s -w" -o dist/oni-view.wasm .
+env CGO_ENABLED=0 GOOS=js GOARCH=wasm go build -ldflags="-s -w" -trimpath -buildvcs=false -o dist/oni-view.wasm .
 
 # Optimize the WebAssembly binary if wasm-opt is available.
 if command -v wasm-opt >/dev/null; then
-  wasm-opt -Oz \
+  wasm-opt -Oz --strip-dwarf --strip-producers\
     --enable-reference-types \
     --enable-bulk-memory \
     --enable-mutable-globals \
