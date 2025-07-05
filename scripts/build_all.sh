bf49bcda646b88f8232797f25f91bfb6ba7eef06
Distortions81 <carlotto81@gmail.com>
2025-07-04 23:37:55 -0600

    faster build

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 mkdir -p dist
-env CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o dist/oni-view-linux-amd64 .
+#env CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o dist/oni-view-linux-amd64 .
 #env CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -o dist/oni-view-darwin-amd64 .
 #env CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build -o dist/oni-view-darwin-arm64 .
-env CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o dist/oni-view-windows-amd64.exe .
+#env CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o dist/oni-view-windows-amd64.exe .
 env CGO_ENABLED=0 GOOS=js GOARCH=wasm go build -ldflags="-s -w" -o dist/oni-view.wasm .
 
 # Optimize the WebAssembly binary if wasm-opt is available.
