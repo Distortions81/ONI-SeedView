97bc8d4aea6e4025a902df49f6f15109e62cab7b
Distortions81 <carlotto81@gmail.com>
2025-07-05 18:37:03 -0600

    trim spaces too

--- asteroid.go
+++ asteroid.go
@@ -5,6 +5,7 @@ import "strings"
 // asteroidIndexByID searches the asteroid slice for the given ID and returns
 // its index, or -1 if not found.
 func asteroidIndexByID(asts []Asteroid, id string) int {
+	id = strings.TrimSpace(id)
 	for i, a := range asts {
 		if strings.EqualFold(a.ID, id) {
 			return i
