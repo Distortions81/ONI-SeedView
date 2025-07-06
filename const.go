e97e4dffdcf3198b19a694d944e736d51bf5bc66
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2025-07-05 20:18:20 -0600

    Remove off-window cursor checks

--- const.go
+++ const.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	ClientVersion    = "v0.0.6-2507060159"
+	ClientVersion    = "v0.0.6-2507060215"
 	BaseURL          = "https://ingest.mapsnotincluded.org/coordinate/"
 	AcceptCBORHeader = "application/cbor"
 	PanSpeed         = 15
