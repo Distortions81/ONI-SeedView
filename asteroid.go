8b44361a403b9898cd9b10320208c04a584b19c5
Distortions81 <carlotto81@gmail.com>
2025-07-05 18:36:42 -0600

    case-insensitive

--- asteroid.go
+++ asteroid.go
@@ -1,10 +1,12 @@
 package main
 
+import "strings"
+
 // asteroidIndexByID searches the asteroid slice for the given ID and returns
 // its index, or -1 if not found.
 func asteroidIndexByID(asts []Asteroid, id string) int {
 	for i, a := range asts {
-		if a.ID == id {
+		if strings.EqualFold(a.ID, id) {
 			return i
 		}
 	}
